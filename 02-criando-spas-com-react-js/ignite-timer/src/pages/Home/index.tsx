604a237e215558d933e08deee2e5aba814c0fd23
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-23 20:01:59 -0300

    Completion of the House

--- 02-criando-spas-com-react-js/ignite-timer/src/pages/Home/index.tsx
+++ 02-criando-spas-com-react-js/ignite-timer/src/pages/Home/index.tsx
@@ -3,7 +3,10 @@ import {
   CountdownContainer,
   FormContainer,
   HomeContainer,
+  MinutesAmountInput,
   Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles'
 
 export function Home() {
@@ -12,9 +15,13 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input id="task" />
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
           <label htmlFor="minutesAmount">durante</label>
-          <input id="minutesAmount" />
+          <MinutesAmountInput
+            id="minutesAmount"
+            placeholder="00:00"
+            type="number"
+          />
           <span>minutos.</span>
         </FormContainer>
 
@@ -25,10 +32,10 @@ export function Home() {
           <span>0</span>
           <span>0</span>
         </CountdownContainer>
-        <button type="submit">
+        <StartCountdownButton type="submit" disabled>
           <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
