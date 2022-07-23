563e51b94c17d22774c9a47fb395826f09313b2c
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-23 00:30:55 -0300

    React Router DOM

--- 02-criando-spas-com-react-js/ignite-timer/src/App.tsx
+++ 02-criando-spas-com-react-js/ignite-timer/src/App.tsx
@@ -1,17 +1,13 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
       <GlobalStyle />
+      <Router />
     </ThemeProvider>
   )
 }
