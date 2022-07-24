4fe95938bc65f6fb3f42b9077f31f2c6eafb86c2
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-23 22:05:32 -0300

    Pages and Routes - Page: History

---
+++ 02-criando-spas-com-react-js/ignite-timer/src/pages/History/styles.ts
@@ -0,0 +1,77 @@
+import styled from 'styled-components'
+
+export const HistoryContainer = styled.main`
+  flex: 1;
+  padding: 3.5rem;
+
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    font-size: 1.5rem;
+    color: ${(props) => props.theme['gray-100']};
+  }
+`
+
+export const HistoryList = styled.div`
+  flex: 1;
+  overflow: auto;
+  margin-top: 2rem;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+    height: 5px;
+    @media (max-width: 780px) {
+      width: 5px;
+      height: 5px;
+    }
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${(props) => props.theme['gray-100']};
+    border-radius: 50px;
+    height: 5px;
+  }
+
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background-color: ${(props) => props.theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${(props) => props.theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
+`
