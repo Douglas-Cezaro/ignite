7599c14d73e2b450879132dc8ec19fe7806940a7
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-24 17:43:47 -0300

    Adding documents in Firestore via code

--- react-native/myshopping/src/components/FormBox/index.tsx
+++ react-native/myshopping/src/components/FormBox/index.tsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useState } from "react";
+import firestore from "@react-native-firebase/firestore";
 
-import { Container } from './styles';
-import { ButtonIcon } from '../ButtonIcon';
-import { Input } from '../Input';
+import { Container } from "./styles";
+import { ButtonIcon } from "../ButtonIcon";
+import { Input } from "../Input";
+import { Alert } from "react-native";
 
 export function FormBox() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(0);
+
+  async function handleProductAdd() {
+    firestore()
+      .collection("products")
+      .add({
+        description,
+        quantity,
+        done: false,
+      })
+      .then(() => {
+        setDescription("");
+        setQuantity(0);
+        Alert.alert("Produtos", "Produto adicionado com sucesso!");
+      })
+      .catch((error) => {
+        console.error(error);
+        Alert.alert("Produtos", "Erro ao adicionar produto.");
+      });
+  }
+
   return (
     <Container>
       <Input
         placeholder="Nome do produto"
         size="medium"
+        value={description}
+        onChangeText={setDescription}
       />
 
       <Input
@@ -17,13 +43,18 @@ export function FormBox() {
         keyboardType="numeric"
         size="small"
         style={{ marginHorizontal: 8 }}
+        value={String(quantity)}
+        onChangeText={(value) => {
+          setQuantity(Number(value));
+        }}
       />
 
       <ButtonIcon
-        size='large'
+        size="large"
         icon="add-shopping-cart"
-        onPress={() => { }}
+        onPress={handleProductAdd}
       />
     </Container>
   );
 }
+
