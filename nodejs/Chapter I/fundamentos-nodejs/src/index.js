336f3dc7a528d0e28fd882111b822ff99e2062db
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-24 21:36:50 -0300

    Using HTTP methods

--- nodejs/Chapter I/fundamentos-nodejs/src/index.js
+++ nodejs/Chapter I/fundamentos-nodejs/src/index.js
@@ -2,8 +2,32 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello, moto!" });
+/**
+ * GET - Buscar uma informação dentro do servidor
+ * POST - Inserir uma informação no servidor
+ * PUT - Alterar uma informação no servidor
+ * PATCH - Alterar uma informação especifica
+ * DELETE - Deletar uma informação no servidor
+ */
+
+app.get("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 2", "Curso 4"]);
 });
 
 app.listen(3333);
