df7a13f35473cd4e9e04dd0063c18b5efd8eeaee
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-07-31 19:52:50 -0300

    Context in React - Date Formatting

--- reactjs/02-criando-spas-com-react-js/ignite-timer/src/pages/History/index.tsx
+++ reactjs/02-criando-spas-com-react-js/ignite-timer/src/pages/History/index.tsx
@@ -1,4 +1,6 @@
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../contexts/CyclesContext'
 import { HistoryContainer, HistoryList, Status, TdEmptyValues } from './styles'
 
@@ -30,7 +32,12 @@ export function History() {
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>
                   <td>
                     {cycle.finishedDate && (
                       <Status statusColor="green">Concluído</Status>
