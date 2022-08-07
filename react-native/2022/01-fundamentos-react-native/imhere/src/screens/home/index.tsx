bfad289c59eef4f1b3a3fc87520f88c6456cb7d5
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-08-07 14:42:29 -0300

    Ending the App - Removing Participants

--- react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
+++ react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
@@ -36,7 +36,12 @@ export function Home() {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: "Sim",
-        onPress: () => Alert.alert("Deletado!"),
+        onPress: () => {
+          setParticipants((prevState) =>
+            prevState.filter((participant) => participant !== name)
+          );
+          Alert.alert("Deletado!");
+        },
       },
       {
         text: "Não",
