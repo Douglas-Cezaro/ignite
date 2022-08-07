75c9736afd97bb01677d8136ec53888121e6dbd7
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-08-06 23:41:47 -0300

    Fundamentals - Styling the StatusBar

--- react-native/2022/01-fundamentos-react-native/imhere/App.tsx
+++ react-native/2022/01-fundamentos-react-native/imhere/App.tsx
@@ -1,10 +1,14 @@
-import { StatusBar } from "expo-status-bar";
+import { StatusBar } from "react-native";
 import { Home } from "./src/screens/home";
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
+      <StatusBar
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
       <Home />
     </>
   );
