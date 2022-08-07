d4edcb79e0342185bff4cd8fd92e54ff64baa57f
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-08-06 23:12:00 -0300

    Fundamentals - Creating the Form

--- react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
+++ react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
@@ -9,15 +9,17 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
-      <TextInput
-        style={styles.input}
-        keyboardAppearance="dark"
-        placeholder="Nome do participante"
-        placeholderTextColor="#6b6b6b"
-      />
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          keyboardAppearance="dark"
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
