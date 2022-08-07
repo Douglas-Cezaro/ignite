69aa46056f694fb9a53f1011383060acf8aa9641
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-08-06 23:08:42 -0300

    Fundamentals - Add Button

--- react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
+++ react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
@@ -1,7 +1,10 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar!");
+  }
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
@@ -12,6 +15,9 @@ export function Home() {
         placeholder="Nome do participante"
         placeholderTextColor="#6b6b6b"
       />
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
