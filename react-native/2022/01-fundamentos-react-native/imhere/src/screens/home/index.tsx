31684d7f91cafdf4dc04652d76dd3a1d50394f6f
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-08-06 23:21:56 -0300

    Fundamentals - Creating Participant Component

--- react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
+++ react-native/2022/01-fundamentos-react-native/imhere/src/screens/home/index.tsx
@@ -1,4 +1,13 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { Participant } from "../../components/Participant";
 import { styles } from "./styles";
 
 export function Home() {
@@ -7,19 +16,30 @@ export function Home() {
   }
   return (
     <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
-      <View style={styles.form}>
-        <TextInput
-          style={styles.input}
-          keyboardAppearance="dark"
-          placeholder="Nome do participante"
-          placeholderTextColor="#6b6b6b"
-        />
-        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-          <Text style={styles.buttonText}>+</Text>
-        </TouchableOpacity>
-      </View>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView behavior="position" enabled>
+          <>
+            <Text style={styles.eventName}>Nome do evento</Text>
+            <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+            <View style={styles.form}>
+              <TextInput
+                style={styles.input}
+                keyboardAppearance="dark"
+                placeholder="Nome do participante"
+                placeholderTextColor="#6b6b6b"
+              />
+              <TouchableOpacity
+                style={styles.button}
+                onPress={handleParticipantAdd}
+              >
+                <Text style={styles.buttonText}>+</Text>
+              </TouchableOpacity>
+            </View>
+            <Participant />
+            <Participant />
+          </>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
     </View>
   );
 }
