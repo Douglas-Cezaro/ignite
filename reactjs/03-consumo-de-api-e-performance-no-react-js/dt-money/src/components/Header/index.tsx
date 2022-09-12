a36b6c9d9f2f66126b8f1e0ea0595f11fd4c8196
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-09-11 22:34:07 -0300

    Visual structure - Creating an accessible modal

--- reactjs/03-consumo-de-api-e-performance-no-react-js/dt-money/src/components/Header/index.tsx
+++ reactjs/03-consumo-de-api-e-performance-no-react-js/dt-money/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransitionButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 import logoImg from "../../assets/logo.svg";
 
 export function Header() {
@@ -6,8 +7,23 @@ export function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} alt="" />
-        <NewTransitionButton>Nova transação</NewTransitionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransitionButton>Nova transação</NewTransitionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
 }
+
