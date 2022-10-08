3da51381e1c18d08e3835741e86938f377e9b55e
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-08 20:36:18 -0300

    Component: Header

--- reactjs/2021/03-fundamentos-do-next/ignews/src/pages/_document.tsx
+++ reactjs/2021/03-fundamentos-do-next/ignews/src/pages/_document.tsx
@@ -16,6 +16,7 @@ export default class MyDocument extends Document {
             rel="stylesheet"
           />
         </Head>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         <body>
           <Main />
           <NextScript />
