fa848afa0f68f2b81ac1b0f33e86659f9660df79
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-09 17:03:04 -0300

    Generating checkout session

--- reactjs/2021/03-fundamentos-do-next/ignews/src/components/SubscribeButton/index.tsx
+++ reactjs/2021/03-fundamentos-do-next/ignews/src/components/SubscribeButton/index.tsx
@@ -1,12 +1,31 @@
+import { useSession, signIn } from "next-auth/react";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+// 3 Lugares para usar as credenciais secretas
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API Routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      onClick={handleSubscribe}
+      type="button"
+      className={styles.subscribeButton}
+    >
       Subscribe now
     </button>
   );
