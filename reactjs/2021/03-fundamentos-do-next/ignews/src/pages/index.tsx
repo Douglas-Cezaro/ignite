f0881b54c460e148c65fe18f92ecc3b01a4e2f30
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-08 23:17:18 -0300

    Consuming Stripe API (SSR)

--- reactjs/2021/03-fundamentos-do-next/ignews/src/pages/index.tsx
+++ reactjs/2021/03-fundamentos-do-next/ignews/src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "../styles/home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,13 +26,29 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publication <br />
-            <span>for $9.90 month.</span>
+            <span>for {product.amount} month.</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1LqoemCqXaBE3cpEP46APorX");
+
+  const product = {
+    priceId: price.id,
+    //DICA PARA ARMAZENAR PRECO NO BANCO, ARMAZENAR EM CENTAVOS PARA SEMPRE SER UM NUMERO INTEIRO
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: { product },
+  };
+};
 
