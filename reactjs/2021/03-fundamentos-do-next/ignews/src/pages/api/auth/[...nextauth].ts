56c7d1f08cb844cd0f9eeeef4187f30470dd6da9
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-09 15:26:38 -0300

    Verificando usuário duplicado

--- reactjs/2021/03-fundamentos-do-next/ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/2021/03-fundamentos-do-next/ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,9 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
-export const authOptions = {
+export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -13,7 +15,30 @@ export const authOptions = {
       },
     }),
   ],
-};
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
 
-export default NextAuth(authOptions);
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+        return true;
+      } catch (e) {
+        return false;
+      }
+    },
+  },
+});
 
