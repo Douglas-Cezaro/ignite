dcfbb14db0042d749f0a2739cee1e37fddf37ae3
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-08 21:15:11 -0300

    Component: SignInButton

--- reactjs/2021/03-fundamentos-do-next/ignews/src/components/Header/index.tsx
+++ reactjs/2021/03-fundamentos-do-next/ignews/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
@@ -15,6 +16,7 @@ export function Header() {
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header>
   );
