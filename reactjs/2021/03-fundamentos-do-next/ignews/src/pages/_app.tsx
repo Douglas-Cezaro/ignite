1bba0bcc400387541b765ee574fb4ad1065da502
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-09 14:19:26 -0300

    Authentication with Next Auth

--- reactjs/2021/03-fundamentos-do-next/ignews/src/pages/_app.tsx
+++ reactjs/2021/03-fundamentos-do-next/ignews/src/pages/_app.tsx
@@ -1,13 +1,21 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
-import "../styles/global.scss";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }: AppProps) {
+import "../styles/global.scss";
+import { Session } from "next-auth";
+
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
+}>) {
   return (
-    <>
+    <SessionProvider session={session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
 
