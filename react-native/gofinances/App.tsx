ee761345625c22e589b0363c45735ba5ff5358bf
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-10-24 23:01:27 -0300

    Custom Fonts Management

--- react-native/gofinances/App.tsx
+++ react-native/gofinances/App.tsx
@@ -1,8 +1,38 @@
+import { useCallback, useEffect } from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
 import { Dashboard } from "./src/pages/Dashboard";
 
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  });
+
+  async function prepare() {
+    await SplashScreen.hideAsync();
+  }
+  useEffect(() => {
+    if (fontsLoaded) {
+      prepare();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <Dashboard />
