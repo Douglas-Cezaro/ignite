6cf6dc11ef154d64633e63164cc3ec785ac91cda
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-11-12 23:10:13 -0300

    Loading product data

--- reactjs/04-fundamentos-do-next/ignite-shop/src/pages/product/[id].tsx
+++ reactjs/04-fundamentos-do-next/ignite-shop/src/pages/product/[id].tsx
@@ -1,30 +1,80 @@
+import { GetStaticProps } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
+import Stripe from "stripe";
+import { shimmer, toBase64 } from "../../components/Shimmer";
+import { stripe } from "../../lib/stripe";
 import {
   ImageContainer,
   ProductContainer,
   ProductDetails,
 } from "../styles/pages/product";
 
-export default function Product() {
-  const { query } = useRouter();
+interface ProductProps {
+  product: {
+    id: string;
+    description: string;
+    name: string;
+    imageUrl: string;
+    price: string;
+  };
+}
 
+export default function Product({ product }: ProductProps) {
   return (
     <ProductContainer>
-      <ImageContainer>{/* <Image /> */}</ImageContainer>
+      <ImageContainer>
+        <Image
+          placeholder="blur"
+          src={product.imageUrl}
+          width={520}
+          height={480}
+          alt=""
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+            shimmer(520, 480)
+          )}`}
+        />
+      </ImageContainer>
       <ProductDetails>
-        <h1>Camiseta X</h1>
-        <span>R$ 79,90</span>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
 
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          repellat modi maxime? In possimus, cupiditate quaerat ipsam
-          consectetur laborum assumenda cum, distinctio, quod voluptatem
-          officiis eligendi dolores qui doloremque cumque!
-        </p>
+        <p>{product.description}</p>
 
         <button>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
 }
+
+export const getStaticProps: GetStaticProps<
+  any,
+  {
+    id: string;
+  }
+> = async ({ params }) => {
+  const productId = params.id;
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ["default_price"],
+  });
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: "BRL",
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hour
+  };
+};
 
