7f1729af9bc60a9ef062de450294bcedf70ede9a
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-11-12 22:32:35 -0300

    Product page structure

--- reactjs/04-fundamentos-do-next/ignite-shop/src/pages/product/[id].tsx
+++ reactjs/04-fundamentos-do-next/ignite-shop/src/pages/product/[id].tsx
@@ -1,8 +1,30 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer>{/* <Image /> */}</ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+          repellat modi maxime? In possimus, cupiditate quaerat ipsam
+          consectetur laborum assumenda cum, distinctio, quod voluptatem
+          officiis eligendi dolores qui doloremque cumque!
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
 
