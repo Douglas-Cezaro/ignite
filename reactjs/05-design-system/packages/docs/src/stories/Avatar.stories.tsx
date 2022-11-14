d034b246a5b13e9cf48b180fd611b3662c751e10
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-11-13 23:08:29 -0300

    Design System Components - Component: Avatar

---
+++ reactjs/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Avatar, AvatarProps } from '@czr-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://avatars.githubusercontent.com/u/53586466?v=4',
+    alt: 'Douglas Cezaro',
+  },
+} as Meta<AvatarProps>
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
