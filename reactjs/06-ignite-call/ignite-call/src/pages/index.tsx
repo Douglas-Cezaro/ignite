b4af573253329cd8708653bbcf0b76d017fc8774
Douglas-Cezaro <cezarodouglas@gmail.com>
2022-12-24 23:49:33 -0300

    Setup & structure - Installation of the Design System

--- reactjs/06-ignite-call/ignite-call/src/pages/index.tsx
+++ reactjs/06-ignite-call/ignite-call/src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Heading } from "@ignite-ui/react";
+
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Heading>Hello World</Heading>
     </div>
   );
 }
