ae47fdb655a2ff7f181fe3f4221176ff7cc052be
Douglas-Cezaro <cezarodouglas@gmail.com>
2023-06-18 16:47:43 -0300

    Calendar and - loading available times

--- reactjs/06-ignite-call/ignite-call/src/pages/api/users/[username]/availability.api.ts
+++ reactjs/06-ignite-call/ignite-call/src/pages/api/users/[username]/availability.api.ts
@@ -56,5 +56,25 @@ export default async function handler(
     },
   )
 
-  return res.json({ possibleTimes })
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true,
+    },
+    where: {
+      user_id: user.id,
+      date: {
+        gte: referenceDate.set('hour', startHour).toDate(),
+        lte: referenceDate.set('hour', endHour).toDate(),
+      },
+    },
+  })
+
+  const availabilityTime = possibleTimes.filter((time) => {
+    // Retorna se pelo menos um item atende a condição passada
+    return !blockedTimes.some(
+      (blockedTime) => blockedTime.date.getHours() === time,
+    )
+  })
+
+  return res.json({ possibleTimes, availabilityTime })
 }
